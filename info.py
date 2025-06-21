873c9993c88b0234d41ed3483e7925162e930ee6
devilbossji <lenovolaptop8583@gmail.com>
2025-06-22 01:38:21 +0530

    Update info.py

--- info.py
+++ info.py
@@ -60,7 +60,7 @@ AUTH_REQ_CHANNEL = int(environ.get('AUTH_REQ_CHANNEL', '-1002459554188'))
 
 IS_VERIFY = is_enabled('IS_VERIFY', True)
 LOG_VR_CHANNEL = int(environ.get('LOG_VR_CHANNEL', '-1002748763541'))
-LOG_API_CHANNEL = int(environ.get('LOG_API_CHANNEL', '--1002748763541'))
+LOG_API_CHANNEL = int(environ.get('LOG_API_CHANNEL', '-1002748763541'))
 VERIFY_IMG = environ.get("VERIFY_IMG", "https://telegra.ph/file/9ecc5d6e4df5b83424896.jpg")
 
 TUTORIAL = environ.get("TUTORIAL", "https://t.me/+sYYpPgyyOq03Mzhl")
